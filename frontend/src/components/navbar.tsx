5d9b253e48430c869b24baf9b421d99fb13640a2
jluims <72709760+jluims@users.noreply.github.com>
2024-07-27 15:15:08 -0400

    consistent name

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,7 +2,7 @@ import classes from "../css/navbar.module.css";
 function Navbar() {
   return (
     <nav className={classes.navbar}>
-      <a href="/" className={classes.title}>PFtracker</a>
+      <a href="/" className={classes.title}>PFtrackr</a>
       <a href="/privacy-policy">Privacy Policy</a>
     </nav>
   );
